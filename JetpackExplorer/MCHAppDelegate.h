7dde6996e75253eb817df4f2a7ddcf7fdcddbde8
Marc Henderson <marchend@gmail.com>
2014-02-10 14:34:39 -0500

    Initial Commit

---
+++ JetpackExplorer/MCHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCHAppDelegate.h
+//  JetpackExplorer
+//
+//  Created by Marc Henderson on 2/10/2014.
+//  Copyright (c) 2014 Marc Henderson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JetpackExplorer/MCHMyScene.h
@@ -0,0 +1,13 @@
+//
+//  MCHMyScene.h
+//  JetpackExplorer
+//
+
+//  Copyright (c) 2014 Marc Henderson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MCHMyScene : SKScene
+
+@end
