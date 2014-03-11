216b21fcee3adc9366114bbd2814c984ad113a7f
Marc Henderson <marchend@gmail.com>
2014-03-11 15:08:32 -0400

    MCH - update of project structure and adds in a texture atlas for sprites

---
+++ JetpackExplorer/sprites/MCHJetpackSprite.h
@@ -0,0 +1,13 @@
+//
+//  MCHJetpackSprite.h
+//  JetpackExplorer
+//
+//  Created by Marc Henderson on 2014-03-11.
+//  Copyright (c) 2014 Marc Henderson. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MCHJetpackSprite : SKSpriteNode
+
+@end
---
+++ JetpackExplorer/sprites/MCHJetpackSprite.m
@@ -0,0 +1,13 @@
+//
+//  MCHJetpackSprite.m
+//  JetpackExplorer
+//
+//  Created by Marc Henderson on 2014-03-11.
+//  Copyright (c) 2014 Marc Henderson. All rights reserved.
+//
+
+#import "MCHJetpackSprite.h"
+
+@implementation MCHJetpackSprite
+
+@end
