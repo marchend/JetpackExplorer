5ddf945b3cb16aaed95835fa4db4f7fc5a279cb2
Marc Henderson <marchend@gmail.com>
2014-03-12 14:57:02 -0400

    MCH - update to make the game a landscape game. update to make the status bar white.

--- JetpackExplorer/scenes/MCHMenuScene.m
+++ JetpackExplorer/scenes/MCHMenuScene.m
@@ -12,6 +12,39 @@
 
 @implementation MCHMenuScene
 
+- (void)addVersionLabel {
+    /*
+     SKLabelNode *playButtonLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
+     playButtonLabel.text = @"NEW GAME";
+     playButtonLabel.fontSize = 28;
+     playButtonLabel.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame) - 30);
+     playButtonLabel.fontColor = [UIColor whiteColor];
+     [self addChild:playButtonLabel];
+     
+     self.playButton = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.frame.size.width, playButtonLabel.frame.size.height+60)];
+     self.playButton.position = CGPointMake(CGRectGetMidX(self.frame),playButtonLabel.position.y+playButtonLabel.frame.size.height/2);
+     
+     SKLabelNode *highScoreButtonLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
+     highScoreButtonLabel.text = @"HIGH SCORES";
+     highScoreButtonLabel.fontSize = 28;
+     highScoreButtonLabel.position = CGPointMake(CGRectGetMidX(self.frame),playButtonLabel.position.y - playButtonLabel.frame.size.height*4);
+     highScoreButtonLabel.fontColor = [UIColor whiteColor];
+     [self addChild:highScoreButtonLabel];
+     
+     self.leaderboardButton = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.frame.size.width, highScoreButtonLabel.frame.size.height+60)];
+     self.leaderboardButton.position = CGPointMake(CGRectGetMidX(self.frame),highScoreButtonLabel.position.y+highScoreButtonLabel.frame.size.height/2);
+     */
+    
+    NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
+    NSString* version = [infoDict objectForKey:@"CFBundleVersion"];
+    SKLabelNode *versionDisplay = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
+    versionDisplay.text = [NSString stringWithFormat:@"version %@",version];
+    versionDisplay.fontSize = 12;
+    versionDisplay.position = CGPointMake(CGRectGetMidX(self.frame),versionDisplay.frame.size.height + 5);
+    versionDisplay.fontColor = [UIColor whiteColor];
+    [self addChild:versionDisplay];
+}
+
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
@@ -30,52 +63,34 @@
         
         SKLabelNode *title = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
         title.text = @"JETPACK EXPLORER";
-        title.fontSize = 38;
+        title.fontSize = 28;
         title.fontColor = [UIColor whiteColor];
-        title.position = CGPointMake(CGRectGetMidX(self.frame),self.frame.size.height - title.frame.size.height * 2);
+        title.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame)+40);
         [self addChild:title];
         
-        /*
-         self.subtitle = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
-         self.subtitle.text = @"(r o b f o rd edition)";
-         self.subtitle.fontSize = 38;
-         self.subtitle.position = CGPointMake(CGRectGetMidX(self.frame),self.title.position.y - self.title.frame.size.height);
-         */
+        self.playButton = [self createMenuButtonWithLabel:@"NEW GAME" atPosition:CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame))];
+        self.leaderboardButton = [self createMenuButtonWithLabel:@"HIGH SCORES" atPosition:CGPointMake(CGRectGetMidX(self.frame),self.playButton.position.y-50)];
         
-        SKLabelNode *playButtonLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
-        playButtonLabel.text = @"NEW GAME";
-        playButtonLabel.fontSize = 28;
-        playButtonLabel.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame) - 30);
-        playButtonLabel.fontColor = [UIColor whiteColor];
-        [self addChild:playButtonLabel];
-        
-        self.playButton = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.frame.size.width, playButtonLabel.frame.size.height+60)];
-        self.playButton.position = CGPointMake(CGRectGetMidX(self.frame),playButtonLabel.position.y+playButtonLabel.frame.size.height/2);
-        
-        SKLabelNode *highScoreButtonLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
-        highScoreButtonLabel.text = @"HIGH SCORES";
-        highScoreButtonLabel.fontSize = 28;
-        highScoreButtonLabel.position = CGPointMake(CGRectGetMidX(self.frame),playButtonLabel.position.y - playButtonLabel.frame.size.height*4);
-        highScoreButtonLabel.fontColor = [UIColor whiteColor];
-        [self addChild:highScoreButtonLabel];
-        
-        self.leaderboardButton = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.frame.size.width, highScoreButtonLabel.frame.size.height+60)];
-        self.leaderboardButton.position = CGPointMake(CGRectGetMidX(self.frame),highScoreButtonLabel.position.y+highScoreButtonLabel.frame.size.height/2);
-        
-        NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
-        NSString* version = [infoDict objectForKey:@"CFBundleVersion"];
-        SKLabelNode *versionDisplay = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
-        versionDisplay.text = [NSString stringWithFormat:@"version %@",version];
-        versionDisplay.fontSize = 12;
-        versionDisplay.position = CGPointMake(CGRectGetMidX(self.frame),versionDisplay.frame.size.height + 5);
-        versionDisplay.fontColor = [UIColor whiteColor];
-        [self addChild:versionDisplay];
+        [self addVersionLabel];
         
         [self addChild:self.playButton];
         [self addChild:self.leaderboardButton];
         
     }
     return self;
+}
+
+-(SKSpriteNode *)createMenuButtonWithLabel:(NSString *)label atPosition:(CGPoint)position{
+    SKLabelNode *buttonLabel = [SKLabelNode labelNodeWithFontNamed:@"Helvetica Neue UltraLight"];
+    buttonLabel.text = label;
+    buttonLabel.fontSize = 18;
+    buttonLabel.position = position;
+    buttonLabel.fontColor = [UIColor whiteColor];
+    [self addChild:buttonLabel];
+    
+    SKSpriteNode *returnNode = [SKSpriteNode spriteNodeWithColor:[UIColor clearColor] size:CGSizeMake(self.frame.size.width, buttonLabel.frame.size.height+40)];
+    returnNode.position = CGPointMake(CGRectGetMidX(self.frame),buttonLabel.position.y+buttonLabel.frame.size.height/2);;
+    return returnNode;
 }
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
